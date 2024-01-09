de584f9d0edd8a29e6e699f983e6709cd73b3e10
Dan <44291807+dandole@users.noreply.github.com>
2024-01-09 14:56:03 -0500

    Create makeall64.sh

---
+++ makeall64.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+for file in "${@:1}"; do
+	if [ -f "$file" ]; then
+		Status="$(dasdls "$file" 2>/dev/null | grep 'CCKD64\|CCKD\|CKD64\|CKD')";
+		if [ "$Status" != "" ]; then
+			CCKD64="$(echo "$Status" | grep 'CCKD64')";
+			if [ "$CCKD64" != "" ]; then
+				echo "     $file" already CCKD64
+			else
+				backup="$file".bak
+				cmd="mv $file $backup"
+				echo "$cmd"
+				cmd="ckd2cckd64 -q $backup $file"
+				echo "$cmd"
+			fi
+		else
+			echo "     $file" not a DASD.
+		fi
+	else
+		echo "     $file" is a directory.
+	fi
+done;
